0c5e52fb3698ac8bdd5753a097be1297734ba9c4
archana1998 <swaminathan.archana11@gmail.com>
2020-08-07 16:40:00 +0530

    Cleaned code

--- helpers/ImageLoader.py
+++ helpers/ImageLoader.py
@@ -3,8 +3,10 @@ import os
 
 def load_images_from_folder(folder):
     images = []
+    filenames = []
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder,filename))
         if img is not None:
             images.append(img)
-    return images
+            filenames.append(filename)
+    return images, filenames
--- new_image_demo.py
+++ new_image_demo.py
@@ -23,13 +23,6 @@ yolo_df2_params = {   "model_def" : "/media/archana/Local/Flipkart GRiD/weights/
 "img_size" : 416,
 "device" : device}
 
-"""yolo_modanet_params = {   "model_def" : "yolo/modanetcfg/yolov3-modanet.cfg",
-"weights_path" : "yolo/weights/yolov3-modanet_last.weights",
-"class_path":"yolo/modanetcfg/modanet.names",
-"conf_thres" : 0.5,
-"nms_thres" :0.4,
-"img_size" : 416,
-"device" : device}"""
 
 
 #DATASET
@@ -56,55 +49,28 @@ if model == 'yolo':
     detectron = YOLOv3Predictor(params=yolo_params)
 
 
-#Faster RCNN / RetinaNet / Mask RCNN
-
-
-
-
-path = '/media/archana/Local/Flipkart GRiD/Amazon Images'
-images = load_images_from_folder(path)
+path = '/media/archana/Local/Flipkart GRiD/Myntra Images'
+images, filenames = load_images_from_folder(path)
 detections = []
+count = 0
 for i in range (len(images)):
     detections.append(detectron.get_detections(images[i]))
-      
-for i in range (len(images)):
-    if len(detections[i]) != 0 :
-            detections[i].sort(reverse=False ,key = lambda x:x[4])
-for i in range (len(images)):
+    
     for x1, y1, x2, y2, cls_conf, cls_pred in detections[i]:
                 
                 
         if(classes[int(cls_pred)]=="short sleeve top" or classes[int(cls_pred)]=="long sleeve top"):
-                    #print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf))           
-
-                
-
-            color = colors[int(cls_pred)]
-                
-            color = tuple(c*255 for c in color)
-            color = (.7*color[2],.7*color[1],.7*color[0])       
-                    
-            font = cv2.FONT_HERSHEY_SIMPLEX   
-            
-            
+          
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            text =  "%s conf: %.3f" % (classes[int(cls_pred)] ,cls_conf)
-                
-            cv2.rectangle(images[i],(x1,y1) , (x2,y2) , color,3)
             y1 = 0 if y1<0 else y1
-            y1_rect = y1-25
-            y1_text = y1-5
+           
+            new_img=images[i][y1:y2,x1-5:x2+20]
+            if(new_img.any()):    
+                cv2.imwrite('Crops/Myntra Images/'+'crop_'+filenames[i]+'.jpg', new_img)         
+                img_id = path.split('/')[-1].split('.')[0]
+            
 
-
-
-                    
-            #remove y2 addition for flipkart
-            new_img=images[i][y1:y2,x1+5:x2]
-            cv2.imwrite('Crops/Amazon Images/'+'crop_'+str(i)+'.jpg', new_img)         
-            #cv2.imshow('Detections',images[i])
-            img_id = path.split('/')[-1].split('.')[0]
-
-                
+print('Images Successfully Cropped and Saved')                
 
                 
      
